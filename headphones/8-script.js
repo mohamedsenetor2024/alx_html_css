eb405b41673c1f6454582d75b7e07cf1b9e5e1b4
mohamedsenetor2024 <mohamedmuhumed001@gmail.com>
2025-06-22 20:00:19 +0300

    Implement a design from scratch To Real Project with html,css and Javascript

---
+++ headphones/8-script.js
@@ -0,0 +1,11 @@
+
+
+// 8-script.js
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.getElementById('hamburger');
+  const navMenu = document.getElementById('nav-menu');
+
+  hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
+});
